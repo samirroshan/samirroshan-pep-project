4edd96d396091c3e831ffc21453c4e22dad4ded8
Samir Roshan <samir.roshan@yahoo.com>
2025-01-29 03:51:42 +0000

    done

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -10,7 +10,7 @@ import io.javalin.http.Context;
 import java.util.List;
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
+ * found in readme.md as well as the test cases. You should  
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
